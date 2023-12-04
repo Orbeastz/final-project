0e1af5b87eafbb59fd63972ff7fe5f0b17152b4c
Kamare <kamareepic@gmail.com>
2023-12-04 07:52:41 -0500

    added nav.js and other .js files needed to make website functional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React from 'react';
+import './App.css'
+import Nav from './components/Nav'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      "This is here just as a placeholder"
-    </div>
+    <>
+    <Header/>
+    <Nav/>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            {/*logo for little Lemon */}
+            <a href='/'>
+                <img src='src/littleLemon.png' alt= 'little lemon logo'/>
+            </a>
+            {/* mobile navbar */}
+            <ul>
+                <div className='bar'></div>
+                <div className='bar'></div>
+                <div className='bar'></div>
+            </ul>
+            {/* desktop navbar */}
+            <ul>
+               <li>
+                <a href='/'>Home</a>
+               </li>
+               <li>
+               <a href='/'>About</a>
+               </li>
+               <li>
+               <a href='/'>Services</a>
+               </li>
+               <li>
+               <a href='/'>Reservations</a>
+               </li>
+               <li>
+               <a href='/'>Menu</a>
+               </li>
+               <li>
+               <a href='/'>Order Online</a>
+               </li>
+               <li>
+               <a href='/'>Login</a>
+               </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;

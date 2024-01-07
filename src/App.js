f681e65aa22c1b6abe5ad5c7abe32099576d4801
Kamare <kamareepic@gmail.com>
2024-01-07 03:34:50 -0500

    fixed around some components to make them make more sense, and fixed some issues with not being able to render properly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import Nav from './components/Nav'
-import Header from './components/Header'
 import Footer from './components/Footer'
-import Landing from './components/Landingpage';
-import BookingForm from './components/BookingForm'
+import Main from './components/Main';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-    <Route index element={<Landing/>} />
-    <Route path="Header" element={<Header/>} />
-    <Route path="Nav" element={<Nav/>} />
-    <Route path="Footer" element={<Footer/>} />
-    </Routes>
-    </BrowserRouter>
-  );
-}
+    <>
+    <Nav />
+    <Main />
+    <Footer />
+    </>
+  )
+  }
 
 export default App;

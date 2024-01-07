bb2aa9dd3d6b74ab04cd7a764be0f696c3a3cf50
Kamare <kamareepic@gmail.com>
2024-01-07 14:47:24 -0500

    added in recipe.js and cards to display on the main webpage, also handled all of the alrts to go along with it

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Main from './components/Main';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <>
     <Nav />
     <Main />
+    <Menu />
     <Footer />
     </>
   )
